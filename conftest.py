cdcb45a214b63fedc4d309648aa92f4db1e8b79c
Aleksandra Figaj-Zdanowska <afigaj@psi.pl>
2023-10-25 16:27:09 +0200

    Dodanie GH Actions_5

--- conftest.py
+++ conftest.py
@@ -4,22 +4,36 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 LOG_FILTERS = ["faker.factory", "urllib3.connectionpool"]
 
+
 def pytest_configure():
     for logger_name in LOG_FILTERS:
         logger = logging.getLogger(logger_name)
         logger.disabled = True
+
+
 class AdminAuth(BaseSettings):
-    model_config = SettingsConfigDict(env_file="../.env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file="../.env",
+        env_file_encoding="utf-8",
+        extra='ignore'
+    )
     username: str
     password: str
 
+
 class EnvSettings(BaseSettings):
-    model_config = SettingsConfigDict(env_file="../.env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(
+        env_file="../.env",
+        env_file_encoding="utf-8",
+        extra='ignore'
+    )
     base_url: str
+
 
 @pytest.fixture
 def admin_auth() -> AdminAuth:
     return AdminAuth()
+
 
 @pytest.fixture
 def env_settings() -> EnvSettings:
